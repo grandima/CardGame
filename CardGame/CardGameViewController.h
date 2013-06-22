57105947ace0f44e5b2683111ceb53ee27bad61b
Dima <grandima@gmail.com>
2013-06-22 21:55:12 +0300

    Initial Commit

---
+++ CardGame/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  CardGame
+//
+//  Created by Dima Medynsky on 22.06.13.
+//  Copyright (c) 2013 Dima Medynsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ CardGame/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  CardGame
+//
+//  Created by Dima Medynsky on 22.06.13.
+//  Copyright (c) 2013 Dima Medynsky. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
